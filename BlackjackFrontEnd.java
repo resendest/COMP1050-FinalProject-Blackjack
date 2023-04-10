269aa42acfa4f014865bdde3ffe32510ec805324
loricon <loricon@wit.edu>
2023-04-10 18:36:25 -0400

    rest of the card pictures

--- BlackjackFrontEnd.java
+++ BlackjackFrontEnd.java
@@ -14,8 +14,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class BlackjackFrontEnd extends Application {
-	
-	private BlackjackBackEnd play;
+
+    private BlackjackBackEnd play;
     private Label player, dealer, message, status, funds, bet;
     private TextField betTF;
     private Button deal, hit, stand, insurance, split, dd, new_game;
@@ -48,22 +48,22 @@ public class BlackjackFrontEnd extends Application {
     private ImageView cQV, sQV, dQV, hQV;
     private ImageView cKV, sKV, dKV, hKV;
     private boolean canDD, canSplit, canInsure;
-    
 
-	
 
-	
-	public void start(Stage primStage) {
-		play = new BlackjackBackEnd();
-		
-		player = new Label("Your Hand: " + play.getPlayerHandValue());
-		dealer = new Label("Dealer Hand: " + play.getDealerHandValue());
+
+
+
+    public void start(Stage primStage) {
+        play = new BlackjackBackEnd();
+
+        player = new Label("Your Hand: " + play.getPlayerHandValue());
+        dealer = new Label("Dealer Hand: " + play.getDealerHandValue());
         betTF = new TextField();
         bet.setText("Enter bet amount");
         deal = new Button("Deal");
         hit = new Button("Hit");
         stand = new Button("Stand");
-        
+
         play.shuffleDeck();
         cardBack = new Image ("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Card_back_01.svg/640px-Card_back_01.svg.png"); 
         cardBackView = new ImageView(cardBack);
@@ -115,6 +115,62 @@ public class BlackjackFrontEnd extends Application {
         s6V = new ImageView(s6);
         d6 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/6_of_diamonds.svg/640px-6_of_diamonds.svg.png");
         d6V = new ImageView(d6);
+        c7 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Poker-sm-248-7c.png/640px-Poker-sm-248-7c.png");
+        c7V = new ImageView(c7);
+        s7 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Poker-sm-218-7s.png/640px-Poker-sm-218-7s.png");
+        s7V = new ImageView(s7);
+        d7 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Poker-sm-238-7d.png/640px-Poker-sm-238-7d.png");
+        d7V = new ImageView(d7);
+        h7 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Poker-sm-228-7h.png/640px-Poker-sm-228-7h.png");
+        h7V = new ImageView(h7);
+        c8 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Poker-sm-247-8c.png/640px-Poker-sm-247-8c.png");
+        c8V = new ImageView(c8);
+        s8 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Poker-sm-217-8s.png/640px-Poker-sm-217-8s.png");
+        s8V = new ImageView(s8);
+        d8 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Poker-sm-237-8d.png/640px-Poker-sm-237-8d.png");
+        d8V = new ImageView(d8);
+        h8 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Poker-sm-227-8h.png/640px-Poker-sm-227-8h.png");
+        h8V = new ImageView(h8);
+        c9 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Poker-sm-246-9c.png/640px-Poker-sm-246-9c.png");
+        c9V = new ImageView(c9);
+        s9 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Poker-sm-216-9s.png/640px-Poker-sm-216-9s.png");
+        s9V = new ImageView(s9);
+        d9 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Poker-sm-236-9d.png/640px-Poker-sm-236-9d.png");
+        d9V = new ImageView(d9);
+        h9 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Poker-sm-226-9h.png/640px-Poker-sm-226-9h.png");
+        h9V = new ImageView(h9);
+        c10 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Poker-sm-245-Tc.png/640px-Poker-sm-245-Tc.png");
+        c10V = new ImageView(c10);
+        s10 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Poker-sm-215-Ts.png/640px-Poker-sm-215-Ts.png");
+        s10V = new ImageView(s10);
+        d10 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Poker-sm-235-Td.png/640px-Poker-sm-235-Td.png");
+        d10V = new ImageView(d10);
+        h10 = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Poker-sm-225-Th.png/640px-Poker-sm-225-Th.png");
+        h10V = new ImageView(h10);
+        cJ = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Jack_of_clubs2.svg/640px-Jack_of_clubs2.svg.png");
+        cJV = new ImageView(cJ);
+        sJ = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Jack_of_spades2.svg/640px-Jack_of_spades2.svg.png");
+        sJV = new ImageView(sJ);
+        dJ = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Jack_of_spades2.svg/640px-Jack_of_spades2.svg.png");
+        dJV = new ImageView(dJ);
+        hJ = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Jack_of_hearts2.svg/640px-Jack_of_hearts2.svg.png");
+        hJV = new ImageView(hJ);
+        cQ = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Queen_of_clubs2.svg/640px-Queen_of_clubs2.svg.png");
+        cQV = new ImageView(cQ);
+        sQ = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Queen_of_spades2.svg/640px-Queen_of_spades2.svg.png");
+        sQV = new ImageView(sQ);
+        dQ = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Queen_of_diamonds2.svg/640px-Queen_of_diamonds2.svg.png");
+        dQV = new ImageView(dQ);
+        hQ = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Queen_of_hearts2.svg/640px-Queen_of_hearts2.svg.png");
+        hQV = new ImageView(hQ);
+        cK = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/King_of_clubs2.svg/640px-King_of_clubs2.svg.png");
+        cKV = new ImageView(cK);
+        sK = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/King_of_spades2.svg/640px-King_of_spades2.svg.png");
+        sKV = new ImageView(sK);
+        dK = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/King_of_diamonds2.svg/640px-King_of_diamonds2.svg.png");
+        dKV = new ImageView(dK);
+        hK = new Image("https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/King_of_hearts2.svg/640px-King_of_hearts2.svg.png");
+        hKV = new ImageView(hK);
         player = new Label("Your hand:");
         dealer = new Label("Dealer hand:");
         status = new Label("Place your bet, then click the New Game button to begin!");
@@ -130,11 +186,11 @@ public class BlackjackFrontEnd extends Application {
         canDD = true;
         canSplit = true;
         canInsure = true;
-        
-        
-        
-        
-		
+
+
+
+
+
         deal.setOnAction(e -> {
             int bet_amount = Integer.parseInt(bet.getText());
             play.getPlayerBet();
@@ -142,20 +198,19 @@ public class BlackjackFrontEnd extends Application {
             updateLabels();
             toggleButtons(true, true, false, false);
         });
-	}
-	
-	public void placeBet(TextField bet) {
-		
-	}
-	
+    }
 
-	
-	public void updateLabels() {
-	}
-	public void toggleButtons(boolean hit, boolean stand, boolean deal, boolean reset) {
-	}
-	public void shuffleDeck() {
-	}
-	}
+    public void placeBet(TextField bet) {
 
+    }
+
+
+
+    public void updateLabels() {
+    }
+    public void toggleButtons(boolean hit, boolean stand, boolean deal, boolean reset) {
+    }
+    public void shuffleDeck() {
+    }
+}
 
